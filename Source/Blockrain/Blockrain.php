9de2625fb5564b8dd7491f97367365332c68cc2f
Justin Grimes <zelon88@gmail.com>
2019-03-25 22:25:14 -0400

    Add $noStyles to lots of games. Improve performance.
    -Add $noStyles = 1; to games missing it.
    -Should improve performance & load times.
    -Bejeweled to v1.1.
    -Pacman-Canvas to v1.1.
    -Hextris to v1.1.
    -Clumsy Bird to v1.1.
    -Mezball to v1.1.
    -Wolf3D to v1.1.
    -3D City to v1.1.
    -HexGL to v1.2.
    -Blockrain to v1.1.

--- Source/Blockrain/Blockrain.php
+++ Source/Blockrain/Blockrain.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <?php
+$noStyles = 1;
 /*//
 HRCLOUD2-PLUGIN-START
 App Name: Blockrain
 App Website: https://github.com/Aerolab/blockrain.js
-App Version: v1.0 (12-4-2018 00:00)
+App Version: v1.1 (3-25-2018 00:00)
 App License: GPLv3
 App Author: Aerolab
 App Genre: Games
